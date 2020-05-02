89094741d4e074987c5695da5730d2b03b8adebc
mechanicallamb <60459171+mechanicallamb@users.noreply.github.com>
2020-05-02 16:19:46 -0400

    Add files via upload

---
+++ shaders/depth_vertex.glsl
@@ -0,0 +1,12 @@
+#version 120
+
+layout (location = 0) in vec3 aPos;
+
+uniform mat4 lightSpaceMatrix; //matrix to make the transform
+uniform mat4 model;
+
+void main(){
+
+	lightspace_position = lightSpaceMatrix * model *vec4(aPos, 1.0); //create a position vector with w = 1.0
+
+}
